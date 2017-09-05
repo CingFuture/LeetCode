cdcbc686f400e7da29967e5f8e4db94c1b852543
CingFuture <153035751@qq.com>
2017-09-05 20:23:53 +0800

    更简洁地方法

--- src/TwoSum_001.java
+++ src/TwoSum_001.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class TwoSum_001{
     public static void main(String args[]){
-        int nums[] = {3,5,7,10};
+        int nums[] = {3,7,10};
         int target = 17;
         System.out.println(Arrays.toString(twoSum(nums,target)));
         System.out.println("-----------------------");
@@ -11,17 +11,17 @@ public class TwoSum_001{
             System.out.println(a);
         }
     }
-    public static int[] twoSum(int []nums,int target){
-        int []result = new int[2];
-        Map <Integer,Integer> map = new HashMap<>();
-        for(int i=0;i<nums.length;i++){
-            if(map.containsKey(target - nums[i])){
-                result[0] = map.get(target - nums[i]);
-                result[1] = i;
-                return result;
-            }
-            map.put(nums[i],i);
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
         }
-        return result;
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement) && map.get(complement) != i) {
+                return new int[] { i, map.get(complement) };
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
     }
 }
